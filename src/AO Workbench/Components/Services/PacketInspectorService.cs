f19567ea48287a948eeb242f694371cee2f88161
bitterskittles <basementk1tt3h@gmail.com>
2013-05-19 20:43:30 +0100

    Fixed PacketInspector usage.

--- src/AO Workbench/Components/Services/PacketInspectorService.cs
+++ src/AO Workbench/Components/Services/PacketInspectorService.cs
@@ -19,6 +19,7 @@ namespace SmokeLounge.AoWorkbench.Components.Services
     using System.Diagnostics.Contracts;
     using System.IO;
 
+    using SmokeLounge.AOtomation.Messaging.Messages;
     using SmokeLounge.AOtomation.Messaging.Serialization;
 
     using StreamReader = SmokeLounge.AOtomation.Messaging.Serialization.StreamReader;
@@ -36,7 +37,7 @@ namespace SmokeLounge.AoWorkbench.Components.Services
 
         public PacketInspectorService()
         {
-            this.packetInspector = new PacketInspector();
+            this.packetInspector = new PacketInspector(typeof(MessageBody));
         }
 
         #endregion
